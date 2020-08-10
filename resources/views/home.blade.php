8584d4e33d6b422548efa02c5c8be5ef239bc0b6
”yutmtsm” <yutmtsm@gmail.com>
2020-08-10 00:52:47 +0000

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in!aaa
                 </div>
             </div>
         </div>
